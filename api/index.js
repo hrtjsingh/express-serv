f575184fde343e5a28e1502cf995b155cb1176b3
Hartej Singh <hartej.singh@digimantra.com>
2022-11-26 14:07:50 +0530

    add

--- api/index.js
+++ api/index.js
@@ -4,9 +4,11 @@ import fetch from "node-fetch";
 import cors from 'cors'
 app.use(cors())
 app.get("/get/mdisk", async (req, res) => {
-  console.log(req.query)
   const rep = await fetch(`https://mdiskplay.com/api/mdisk-info/${req.query.id}`)
   res.json(await rep.json())
+})
+app.get("/", async (req, res) => {
+  res.json("running")
 })
 app.listen(4001, () => {
   console.log("listing")
