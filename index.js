5e3c377a37decff5a4492c4aa416ec1569c41f40
hrtjsingh <42902290+hrtjsingh@users.noreply.github.com>
2023-05-21 11:46:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ import fetch from "node-fetch";
 import cors from 'cors'
 app.use(cors())
 app.get("/get/mdisk", async (req, res) => {
-  const rep = await fetch(`https://mdiskplay.com/api/mdisk-info/${req.query.id}`)
+  let url = ''
+  if (req.query.type === "terabox") {
+    url = "https://core.mdiskplay.com/box/terabox"
+  }else if(req.query.type === "pdisk"){
+    url = "https://mdiskplay.com/api/pdisk-info"
+  }else{
+    url = "https://mdiskplay.com/api/mdisk-info"
+  }
+  const rep = await fetch(`${url}/${req.query.id}`)
   res.json(await rep.json())
 })
 app.get("/", async (req, res) => {
