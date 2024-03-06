4bf32f4ad60ec5ba848927b1c5005ea48dd246b1
Hartej <hrtjsingh@gmail.com>
2024-03-06 17:11:34 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import express from 'express'
-const app = express();
+import express from 'express';
+import { fileURLToPath } from 'url';
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 import fetch from "node-fetch";
 import cors from 'cors'
+
+const app = express();
 app.use(cors())
+
 app.get("/get/mdisk", async (req, res) => {
   let url = ''
   if (req.query.type === "terabox") {
     url = "https://core.mdiskplay.com/box/terabox"
-  }else if(req.query.type === "pdisk"){
+  } else if (req.query.type === "pdisk") {
     url = "https://mdiskplay.com/api/pdisk-info"
-  }else{
+  } else {
     url = "https://mdiskplay.com/api/mdisk-info"
   }
   const rep = await fetch(`${url}/${req.query.id}`)
@@ -18,6 +25,50 @@ app.get("/get/mdisk", async (req, res) => {
 app.get("/", async (req, res) => {
   res.json("running")
 })
-app.listen(4001, () => {
-  console.log("listing")
-})
+
+app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = 4001;
+
+// Directory to save downloaded PDFs
+const downloadsDir = path.join(__dirname, 'downloads');
+
+// Create the downloads directory if it doesn't exist
+if (!fs.existsSync(downloadsDir)) {
+  fs.mkdirSync(downloadsDir);
+}
+
+// Endpoint to download and save PDF
+app.post('/download-pdf', async (req, res) => {
+  const { pdfUrl } = req.body;
+
+  try {
+    // Fetch PDF file from the provided URL
+    const response = await axios.get(pdfUrl, {
+      responseType: 'stream'
+    });
+
+    // Generate a unique filename for the downloaded PDF
+    const filename = `${uuidv4()}.pdf`;
+    const filePath = path.join(downloadsDir, filename);
+
+    // Create a writable stream to save the PDF file
+    const writer = fs.createWriteStream(filePath);
+    response.data.pipe(writer);
+
+    // Return the URL of the saved PDF file
+    writer.on('finish', () => {
+      const serverPdfUrl = `${req.protocol}://${req.get('host')}/downloads/${filename}`;
+      res.json({ serverPdfUrl });
+    });
+  } catch (error) {
+    console.error('Error downloading PDF:', error);
+    res.status(500).json({ error: 'Failed to download PDF' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
